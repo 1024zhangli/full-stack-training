1f154370e1ff9df0011ff04298c568b1e9dfa6db
zhangli <1024zhangli@gmail.com>
2017-12-03 17:50:51 +0800

    node文档更新

--- codeReview/2017-11-13/node起服务.md
+++ codeReview/2017-11-13/node起服务.md
@@ -1,4 +1,4 @@
-### 常用步骤
+### 起一个http服务
 ```javascript
 let server=http.createServer((req, res)=>{
   req.url/method/host/...
@@ -16,3 +16,9 @@ server.listen(8080);
 fs.readFile(path, (err, data)=>{});
 fs.writeFile(path, content, err=>{});
 ```
+
+### websocket
+1. 服务端引入socket.io；需要监听httpServer
+2. 客户端引入ip:port/socket.io/socket.io.js，连接到服务器端：io.connect('ws://localhost:8080/');
+3. 客户端发送消息用sock.emit(消息名，参数1，参数2，参数3....)
+4. 服务端接受消息sock.on(消息名， function(参数1，参数2，参数){})
--- codeReview/2017-11-13/ws/mysqltest.js
+++ codeReview/2017-11-13/ws/mysqltest.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql');
 
-//1. 连接，创建一个连接池
-let db = mysql.createConnection({host: 'localhost', user: 'root', password:'', database: 'test'});
-// let db = mysql.createPool({host: 'localhost', user: 'root', password:'', database: 'test'});
+//1. 连接，
+// let db = mysql.createConnection({host: 'localhost', user: 'root', password:'', database: 'test'});
+// 正常应用时应该是创建一个连接池
+let db = mysql.createPool({host: 'localhost', user: 'root', password:'', database: 'test'});
 
 db.query('SELECT * FROM t_user_info t WHERE t.age > 13', (err, data)=> {
   if (err) {
